d98aaadfec58bbe30285ef359eb07be01ccaf2fb
HarryHoan <thongsieusao3@gmail.com>
2024-10-04 23:20:10 +0700

    adding document form for the controllers

--- Application/ViewModels/UserDTO/cLoginUserDTO.cs
+++ Application/ViewModels/UserDTO/cLoginUserDTO.cs
@@ -8,7 +8,7 @@ namespace Application.ViewModels.UserDTO
 {
     public class cLoginUserDTO
     {
-        public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
