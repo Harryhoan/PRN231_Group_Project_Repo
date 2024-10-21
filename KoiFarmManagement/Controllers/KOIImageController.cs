d3217fb080012b31954dde71ed398ce273edc536
chechminh19 <meruem0812@gmail.com>
2024-10-21 12:26:24 +0700

    Update-product-image-byAdmin
    add

--- KoiFarmManagement/Controllers/KOIImageController.cs
+++ KoiFarmManagement/Controllers/KOIImageController.cs
@@ -5,6 +5,7 @@ using Domain.Entities;
 using Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
 
@@ -74,5 +75,47 @@ namespace KoiFarmManagement.Controllers
             await _context.SaveChangesAsync();
             return Ok(new { imageUrls = uploadedImageUrls });
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{productId}/images/{imageId}")]
+        public async Task<IActionResult> cUpdateProductImage(int productId, int imageId, IFormFile file)
+        {
+            var product = await _context.Kois.FindAsync(productId);
+            if (product == null)
+                return NotFound("Product not found");
+
+            var productImage =
+                await _context.Images.FirstOrDefaultAsync(pi => pi.KoiId == productId && pi.Id == imageId);
+            if (productImage == null)
+                return NotFound("Product image not found");
+
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation().Crop("fill").Gravity("face")
+                    };
+                    uploadResult = await _cloudinary.UploadAsync(uploadParams);
+                }
+            }
+
+            if (uploadResult.Url == null)
+                return BadRequest("Could not upload image");
+
+            // Update the image URL in the database
+            productImage.ImageUrl = uploadResult.Url.ToString();
+            await _context.SaveChangesAsync();
+
+            return Ok(new { imageUrl = productImage.ImageUrl });
+        }
+
+
+
+
     }
 }
