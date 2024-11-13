905bd8e9aed9f597b66ac9b300ca301e8184f04f
HarryHoan <thongsieusao3@gmail.com>
2024-11-13 16:07:37 +0700

    fix

---
+++ Infrastructure/Migrations/20241113090633_AddingAddressId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddingAddressId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "addressId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "addressId",
+                table: "Orders");
+        }
+    }
+}
